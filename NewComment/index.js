e4be7992b4dfc98eec9365afa6527d51a051075d
Christian Bär <teggno@hotmail.com>
2018-09-28 15:58:45 -1000

    Added call to validate recaptcha

--- NewComment/index.js
+++ NewComment/index.js
@@ -1,55 +1,71 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
-    if (!validateBodyIsComment(req.body)){
-        const sampleJson = JSON.stringify({ 
-            captchaToken: "whateverToken", 
-            postUrl: "someUrl", 
-            authorName: "Michael Knight",
-            text: "Some valuable text"});
+const recaptcha = require("./recaptcha").default;
 
-        context.res = {
-            status: 400,
-            body: "Request body must be some JSON like " + sampleJson
-        };
-        return;
-    }
-    if (!await verifyCaptcha(req.body.captchaToken)){
-        context.res = {
-            status: 400,
-            body: "Captcha verification failed."
-        };
-        return;
-    }
-
-    const rowKey = context.bindingData.sys.randGuid;
-    const entity = createEntity(req.body, rowKey);
-    context.bindings.commentsTableBinding = [entity];
-
+module.exports = async function(context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
+  if (!validateBodyIsComment(req.body)) {
     context.res = {
-        status: 201, // Created
-        body: { rowKey: rowKey },
-        headers: {
-            "Content-Type": "application/json"
-        }
+      status: 400,
+      body: getMalformedRequestErrorMessage()
     };
+    return;
+  }
+  if (
+    !getSetting("recaptcha_skip") &&
+    !(await verifyCaptcha(req.body.captchaToken))
+  ) {
+    context.res = {
+      status: 400,
+      body: "Captcha rejected."
+    };
+    return;
+  }
+
+  const rowKey = context.bindingData.sys.randGuid;
+  const entity = createEntity(req.body, rowKey);
+  context.bindings.commentsTableBinding = [entity];
+
+  context.res = {
+    status: 201, // Created
+    body: { rowKey: rowKey },
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
 };
 
-async function verifyCaptcha(token){
-    return true;
+async function verifyCaptcha(token) {
+  const verify = recaptcha(
+    getSetting("recaptcha_url"),
+    getSetting("recaptcha_secret")
+  );
+  const result = await verify(token);
+  return !!result.success;
 }
 
-function validateBodyIsComment(body){
-    return body
-        && body.postUrl
-        && body.text
-        && body.authorName;
+function getSetting(name) {
+  return process.env[name];
 }
 
-function createEntity(body, rowKey){
-    return { 
-        PartitionKey: body.postUrl,
-        RowKey: rowKey,
-        text: body.text,
-        authorName: body.authorName
-    }
+function getMalformedRequestErrorMessage() {
+  const sampleJson = JSON.stringify({
+    captchaToken: "whateverToken",
+    postUrl: "someUrl",
+    authorName: "Michael Knight",
+    text: "Some valuable text"
+  });
+
+  return "Request body must be some JSON like " + sampleJson;
+}
+
+function validateBodyIsComment(body) {
+  return body && body.postUrl && body.text && body.authorName;
+}
+
+function createEntity(body, rowKey) {
+  return {
+    PartitionKey: body.postUrl,
+    RowKey: rowKey,
+    text: body.text,
+    authorName: body.authorName
+  };
 }
---
+++ NewComment/recaptcha.js
@@ -0,0 +1,34 @@
+const request = require("https").request;
+
+module.exports = function newCaptchaVerifier(googleUrl, secret) {
+  return async function(token, remoteIP) {
+    let requestBody = `secret=${secret}&response=${token}`;
+    if (remoteIP) requestBody += `&remoteip=${remoteIP}`;
+    const postOptions = {
+      path: googleUrl,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Content-Length": Buffer.byteLength(requestBody)
+      }
+    };
+
+    const verficationResponse = await promisedRequest(postOptions, requestBody);
+    const result = JSON.parse(verficationResponse);
+    return result;
+  };
+};
+
+function promisedRequest(options, requestBody) {
+  return new Promise((resolve, reject) => {
+    const req = request(options, response => {
+      let responseBody = "";
+
+      response.on("data", chunk => (responseBody += chunk));
+      response.on("end", () => resolve(responseBody));
+      response.on("error", err => reject(error));
+    });
+    if (typeof requestBody !== "undefined") req.write(requestBody);
+    req.end();
+  });
+}
