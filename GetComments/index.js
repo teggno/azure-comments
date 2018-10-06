678835d479908583357c4b72a94cb61fe749961b
Christian Bär <teggno@hotmail.com>
2018-10-05 18:48:52 -1000

    Removed email from the response from GetComments because it's the unauthenticated API.

--- GetComments/index.js
+++ GetComments/index.js
@@ -1,22 +1,23 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+module.exports = async function(context, req) {
+  context.log("JavaScript HTTP trigger function processed a request.");
 
-    if (!context.bindings.commentsTableBinding){
-        return;
+  if (!context.bindings.commentsTableBinding) {
+    return;
+  }
+
+  context.res = {
+    // NOTE: the email is deliberately excluded here because this is the
+    // unauthenticated API for getting the comment list.
+    body: context.bindings.commentsTableBinding.map(src => ({
+      postUrl: src.PartitionKey,
+      rowKey: src.RowKey,
+      authorName: src.authorName,
+      text: src.text,
+      parentRowKey: src.parentRowKey,
+      createdTimestampUtc: src.createdTimestampUtc
+    })),
+    headers: {
+      "Content-Type": "application/json"
     }
-
-    context.res = {
-        body: context.bindings.commentsTableBinding.map(src => ({
-            postUrl: src.PartitionKey,
-            rowKey: src.RowKey,
-            authorName: src.authorName,
-            text: src.text,
-            email: src.email,
-            parentRowKey: src.parentRowKey,
-            createdTimestampUtc: src.createdTimestampUtc
-        })),
-        headers: {
-            "Content-Type": "application/json"
-        }
-    };
+  };
 };
